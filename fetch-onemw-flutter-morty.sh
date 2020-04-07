0e302cf7c8ac16b2defcc38831c85dd6f5f6f4cd
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-04-07 16:29:58 +0200

    Update

--- fetch-onemw-flutter-morty.sh
+++ fetch-onemw-flutter-morty.sh
@@ -5,30 +5,32 @@ set -e
 # ONEMW flutter source fetcher and build script by Damian Wrobel
 #
 # Author: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
-# Version: v0.1
+# Version: v0.2
 
 # Assumes you have "onemw-gerrit.sh" somewhere in your $PATH
 
 
 git clone ssh://gerrit.onemw.net:29418/onemw-manifests
-(cd onemw-manifests; onemw-gerrit.sh onemw-manifests:38269) # Use LG manifests from "this" repository" (DON'T MERGE)
 (cd onemw-manifests; onemw-gerrit.sh onemw-manifests:42838) # ONEM-12220 ONEM-12398 Adds support for yocto 2.2 (xml)
-(cd onemw-manifests; onemw-gerrit.sh onemw-manifests:43380) # ONEM-12220 ONEM-12398 Adds support for multiple yocto versions
 (cd onemw-manifests; onemw-gerrit.sh onemw-manifests:45948) # ONEM-10964 Add meta-browser and update meta-clang layer
+(cd onemw-manifests; sed -i 's#ssh://gerrit.onemw.net:29418/onemw-manifests#file://${PWD}#' *.conf.*) # Use LG manifests from "this" repository"
+(cd onemw-manifests; sed -i 's/\(repo sync\)/\1 -j$\(getconf _NPROCESSORS_ONLN\)/g'   common_build.sh)  # Speed up repo tool a little bit
+(cd onemw-manifests; sed -i 's/\(repo forall\)/\1 -j$\(getconf _NPROCESSORS_ONLN\)/g' common_build.sh)  # Speed up repo tool a little bit
 
 # this time it's for EOS 18.3
-(cd onemw-manifests; ./eos-efl-demo-build.sh --yocto-version=2.2 usr18.3)
+(cd onemw-manifests; ./eos-efl-demo-build.sh usr18.3_oe22)
 
 cherry_picks=(
     meta-lgi-eos:45946       # ONEM-10964 Enable meta-browser & meta-clang repository
     meta-lgi-om-common:47525 # ONEM-10964 Tweak gtk+ dependency for chromium
-    meta-lgi-om-common:47712 # ONEM-10964 Import libedit recipe
     meta-lgi-om-common:48178 # ONEM-10964 nss: update to 3.28.1
     meta-lgi-om-common:49322 # ONEM-10964 Prevent programs run as root to crash
     meta-lgi-om-common:51256 # ONEM-10964 Enable building glfw shared library
-    meta-lgi-om-common:52457 # ONEM-10964 Introduce elfio library
     meta-lgi-om-common:50940 # ONEM-10964 Flutter Engine - beautiful mobile apps (POC)
 )
+
+LOG=$PWD/cherry-pick.log
+rm -f $LOG
 
 for i in ${cherry_picks[@]}; do
   IFS=: read -a args <<< $i
@@ -36,7 +38,7 @@ for i in ${cherry_picks[@]}; do
   if [ "$repo" = "onemw-src" ]; then # special case for onemw-src location
     repo="$repo/onemw-src"
   fi
-  (cd onemw/$repo; onemw-gerrit.sh $i)
+  (cd onemw/$repo; onemw-gerrit.sh $i || echo "Failed to apply: $i" >> $LOG)
 done
 
 cat << 'EOF' > onemw/_build.sh
