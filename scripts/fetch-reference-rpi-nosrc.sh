3d4b239a6e0b4639326b08b4565cc3075862a381
Stefan Verkoyen <sverkoyen.contractor@libertyglobal.com>
2020-07-01 10:12:42 +0200

    Update fetch-reference-rpi-nosrc.sh: some changes merged

--- scripts/fetch-reference-rpi-nosrc.sh
+++ scripts/fetch-reference-rpi-nosrc.sh
@@ -12,12 +12,8 @@ repo sync --no-clone-bundle -j$(getconf _NPROCESSORS_ONLN)
 ##### cherry picks
 ## RDKCMF-8631 Add ocdm and playready packageconfigs for aamp
 (cd meta-rdk-video; git fetch "https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-rdk-video" refs/changes/94/40594/1 && git cherry-pick FETCH_HEAD)
-## RDKCMF-8631 Enable ocdm and playready packagecfgs for aamp in reference.inc
-(cd meta-cmf-video-restricted; git fetch "https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-restricted" refs/changes/39/40639/1 && git cherry-pick FETCH_HEAD)
 ## RDKCMF-8640 Enable gold linker as default
 (cd meta-rdk; git fetch "https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-rdk" refs/changes/87/38887/2 && git cherry-pick FETCH_HEAD)
-## RDKCMF-8631 Reference image fixes
-(cd meta-cmf-video-restricted; git fetch "https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-restricted" refs/changes/55/40755/2 && git cherry-pick FETCH_HEAD)
 ## RDKCMF-8631 Support RDK_ENABLE_REFERENCE_IMAGE to enable reference image features
 (cd meta-cmf-raspberrypi; git fetch "https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-raspberrypi" refs/changes/67/40867/1 && git cherry-pick FETCH_HEAD)
 
