fe2e46d95b661263d9a7648cd0468c38be21d338
Stefan Verkoyen <sverkoyen.contractor@libertyglobal.com>
2020-09-02 09:43:14 +0200

    Update fetch-reference-7218c.sh: wlan package not needed anymore

--- scripts/fetch-reference-7218c.sh
+++ scripts/fetch-reference-7218c.sh
@@ -69,7 +69,6 @@ download_list=(
     #  - to: is either dst directory or dst path name
 
     "$TGZS_DIR/refsw_release_unified_URSR_19.2.1_20200201.tgz#downloads/"
-    "$TGZS_DIR/refsw_release_unified_URSR_19.2.1_20200203-wlan_1_20_1.tgz#downloads/"
     "$TGZS_DIR/stblinux-4.9-1.15.tar.bz2#downloads/"
     "$TGZS_DIR/applibs_release_DirectFB_hal-1.7.6.src-2.1.tgz#downloads/"
     "$TGZS_DIR/refsw_release_unified_URSR_19.2.1_20200201_3pips_libertyglobal.tgz#downloads/refsw_release_unified_URSR_19.2.1_20200201_3pip_broadcom.tgz"
