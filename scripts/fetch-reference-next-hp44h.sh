b2a37873dc4a722bcf53093cadafc0e29bf0b989
Stefan Verkoyen <sverkoyen.contractor@libertyglobal.com>
2021-08-06 09:23:17 +0000

    Update hp44h build script

--- scripts/fetch-reference-next-hp44h.sh
+++ scripts/fetch-reference-next-hp44h.sh
@@ -7,46 +7,37 @@ rm -rf hp44h_reference_next$TARGET_DIR_SUFFIX
 mkdir hp44h_reference_next$TARGET_DIR_SUFFIX
 cd hp44h_reference_next$TARGET_DIR_SUFFIX
 
-repo init --no-clone-bundle -u https://code.rdkcentral.com/r/manifests -b rdk-next -m rdkv-nosrc.xml
-repo sync --no-clone-bundle -j$(getconf _NPROCESSORS_ONLN)
+checkout_repo() {
+  if [ ! -d $2 ] ; then
+     git clone $1/$2
+  fi
+  cd $2
+  git checkout $3
+  cd -
+}
+
+repo init -u https://code.rdkcentral.com/r/collaboration/soc/amlogic/aml-accel-manifests -b rdk-next -m rdk-firebolt-dunfell-ref-sc2-k54.xml
+repo sync --no-clone-bundle -j12
 
 ## clone repos not yet in manifest
-
-git clone "ssh://stefan.verkoyen@dev.caldero.com:29418/meta-rdk-skyworth-hx4x"
-cd meta-rdk-skyworth-hx4x
-git checkout rdk-next
-cd -
-
-git clone "https://code.rdkcentral.com/r/collaboration/soc/amlogic/yocto_oe/layers/meta-rdk-aml"
-cd meta-rdk-aml
-git checkout rdk-next
-cd -
-
-git clone "https://code.rdkcentral.com/r/collaboration/soc/amlogic/yocto_oe/layers/meta-rdk-bsp-amlogic"
-cd meta-rdk-bsp-amlogic
-git checkout amlogic-ref-rdkservices
-cd -
-
-git clone "https://code.rdkcentral.com/r/collaboration/soc/amlogic/yocto_oe/layers/meta-amlogic"
-cd meta-amlogic
-git checkout amlogic-ref-rdkservices
-cd -
-
-git clone "https://code.rdkcentral.com/r/collaboration/oem/skyworth/yocto_oe/layers/meta-rdk-oem-skyworth-aml905X2"
-cd meta-rdk-oem-skyworth-aml905X2
-git checkout sc2-rdkservices
-cd -
+########## Skyworth sources ######################################
+checkout_repo "ssh://dev.caldero.com:29418" "meta-rdk-skyworth-hx4x" rdk-next
+checkout_repo "https://code.rdkcentral.com/r/collaboration/oem/skyworth/yocto_oe/layers" "meta-rdk-oem-skyworth-aml905X2" sc2-rdkservices
 
 ## avoid removal of clearkey DISTRO feature
 sed -i 's#DISTRO_FEATURES_remove = " compositor clearkey"#DISTRO_FEATURES_remove = "compositor"#' meta-rdk-bsp-amlogic/conf/machine/include/amlogic_32b.inc
 
-##### cherry picks
-cd meta-amlogic
-git fetch https://code.rdkcentral.com/r/collaboration/soc/amlogic/yocto_oe/layers/meta-amlogic refs/changes/39/57939/2 && git cherry-pick FETCH_HEAD
+cd meta-cmf-video-reference
+git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-reference refs/changes/08/60508/2 && git cherry-pick FETCH_HEAD
+cd -
+cd meta-cmf-video-reference-next
+git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-reference-next refs/changes/75/60675/1 && git cherry-pick FETCH_HEAD
 cd -
 
 cat <<EOF >> _build.sh
-declare -x MACHINE="mesonsc2-hp44h-rdk"
+[ -f /opt/rh/devtoolset-7/enable ] && source /opt/rh/devtoolset-7/enable
+
+declare -x MACHINE="mesonsc2-lib32-hp44h-rdk"
 . meta-cmf-video-reference-next/setup-environment
 EOF
 
