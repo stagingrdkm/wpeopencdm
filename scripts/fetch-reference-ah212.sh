653fccc24d60dc56e90429a6e8a79ffa8cf3efda
Stefan Verkoyen <sverkoyen.contractor@libertyglobal.com>
2021-08-24 07:13:02 +0000

    Update hp44h and ah212 build scripts

--- scripts/fetch-reference-ah212.sh
+++ scripts/fetch-reference-ah212.sh
@@ -19,16 +19,11 @@ checkout_repo() {
 repo init -u https://code.rdkcentral.com/r/collaboration/soc/amlogic/aml-accel-manifests -b rdk-next -m rdk-firebolt-dunfell-ref-sc2-k54.xml
 repo sync --no-clone-bundle -j12
 
-## Fix some manifest versions, we need newer sources
-checkout_repo https://code.rdkcentral.com/r/a/components/generic/rdk-oe meta-cmf-video-reference ee0cdb748d1f2c29f951e54bba31988a8cc4c889
+## take newest sources for reference image
+checkout_repo https://code.rdkcentral.com/r/a/components/generic/rdk-oe meta-cmf-video-reference master
 
 ## avoid removal of clearkey DISTRO feature
 sed -i 's#DISTRO_FEATURES_remove = " compositor clearkey"#DISTRO_FEATURES_remove = "compositor"#' meta-rdk-bsp-amlogic/conf/machine/include/amlogic*.inc
-
-## cherry picks
-cd meta-cmf-video-reference
-git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-reference refs/changes/78/61078/2 && git cherry-pick FETCH_HEAD
-cd -
 
 cat <<EOF >> _build.sh
 [ -f /opt/rh/devtoolset-7/enable ] && source /opt/rh/devtoolset-7/enable
