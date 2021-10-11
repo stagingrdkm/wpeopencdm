5ed5e4c3ff182b8aff190771ae180f21213ae391
Stefan Verkoyen <sverkoyen.contractor@libertyglobal.com>
2021-10-11 07:53:09 +0000

    Add rtd1319 realtek build scripts

---
+++ scripts/fetch-reference-rtd1319.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+TARGET_DIR_SUFFIX=""
+ 
+######### Build setup and repo sync
+rm -rf rtd1319_reference$TARGET_DIR_SUFFIX
+mkdir rtd1319_reference$TARGET_DIR_SUFFIX
+cd rtd1319_reference$TARGET_DIR_SUFFIX
+
+repo init --no-clone-bundle -u https://code.rdkcentral.com/r/manifests -b dunfell -m rdkv-nosrc.xml
+repo sync --no-clone-bundle -j12
+
+## clone meta-rdk-soc-realtek but only shallow because connection sometimes very slow
+## do regular clone if you want to inspect other branches and history
+git clone ssh://gitsrv.realtek.com:29418/git/DHC_SDK/cmf/rdk/yocto_oe/layers/meta-rdk-soc-realtek --branch release/common/rdkv-2021q1-dunfell --depth 1 --single-branch meta-rdk-soc-realtek
+
+cat <<EOF >> _build.sh
+[ -f /opt/rh/devtoolset-7/enable ] && source /opt/rh/devtoolset-7/enable
+
+declare -x MACHINE="mediabox"
+source meta-cmf-video-reference/setup-environment
+EOF
+
+echo "RUN FOLLOWING TO PREPARE FOR BUILD: cd rtd1319_reference$TARGET_DIR_SUFFIX; source _build.sh"
