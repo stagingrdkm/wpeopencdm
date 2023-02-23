4f4eaa8df9627d385cbbcc727065e81bc3abf0b8
Peter Trekels <peter.trekels@androme.com>
2023-02-23 16:51:23 +0100

    Update fetch-reference-next-m393.sh to build normal DAC reference build

--- scripts/fetch-reference-next-m393.sh
+++ scripts/fetch-reference-next-m393.sh
@@ -31,9 +31,6 @@ checkout_repo() {
   cd -
 }
 
-#repo init --no-clone-bundle -u https://code.rdkcentral.com/r/manifests -b dunfell -m rdkv-nosrc.xml
-#repo sync --no-clone-bundle -j12
-
 repo init -u https://code.rdkcentral.com/r/collaboration/soc/broadcom/manifests -m reference/manifest-next-dunfell.xml
 repo sync --no-clone-bundle -j$(getconf _NPROCESSORS_ONLN)
 
@@ -42,9 +39,9 @@ cp .repo/manifests/reference/auto.conf ./rdkmanifests/auto.conf
 cp .repo/manifests/reference/cmf_revision.txt ./rdkmanifests/cmf_revision.txt
 
 checkout_repo https://code.rdkcentral.com/r/collaboration/oem/sagemcom/yocto_oe/layers meta-rdk-oem-sagemcom-m393genac meta-rdk-sagemcom rdk-next 
-#checkout_repo https://code.rdkcentral.com/r/collaboration/soc/broadcom/yocto_oe/layers meta-rdk-broadcom-next meta-rdk-broadcom-generic-rdk rdk-next 
-#checkout_repo https://code.rdkcentral.com/r/collaboration/soc/broadcom/yocto_oe/layers meta-rdk-soc-broadcom meta-rdk-soc-broadcom stable2 
-#mv meta-rdk-oem-sagemcom-m393genac meta-rdk-sagemcom
+
+# patch needed for m393
+(cd meta-cmf-video-reference && git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-reference refs/changes/06/81206/1 && git cherry-pick FETCH_HEAD)
 
 #mkdir rdkmanifests
 #cp .repo/manifests/reference/auto.conf ./rdkmanifests/auto.conf
@@ -96,9 +93,6 @@ done
 # remove sagemcom bbappend about stblinux tarballs
 rm meta-rdk-sagemcom/meta-sagemcom-generic/recipes-kernel/stblinux/stblinux_5.4-1.7-generic-rdk.bbappend
 
-# remove sysint bbappend because patch does not apply (is now commented out in bbappend)
-#rm meta-rdk-sagemcom/meta-sagemcom-top-m393-72180ZB-stb/recipes-extended/sysint/sysint_git.bbappend
-
 # fix refsw tarball reference and checksums
 cat <<EOF >> meta-rdk-sagemcom/meta-sagemcom-generic/recipes-bsp/broadcom-refsw/broadcom-refsw_unified-22.0.1-generic-rdk.bbappend
 SRC_URI_remove = "https://\${RDK_ARTIFACTS_URL}/broadcom_restricted/22_0_1/refsw_release_unified_URSR_22_0_1_20220411_3pips_comcast/2.1/refsw_release_unified_URSR_22_0_1_20220411_3pips_comcast-2.1.tgz;name=3pip"
@@ -113,33 +107,14 @@ SRC_URI[ursr.md5sum] = "4738f2f3ee2f831cf87a015a774769b9"
 SRC_URI[ursr.sha256sum] = "d5e22d4d5250bc9672fb98691680cac321c5ca73f87e3d8ef1546d9875696498"
 EOF
 
-# fix small patch issue in bluez
-#sed -i "s/STB_lowpowermode_5.45.patch'/STB_lowpowermode_5.45.patch;apply=no'/" meta-rdk-broadcom-generic-rdk/meta-brcm-refboard/meta-rdk-ext/recipes-connectivity/bluez5/bluez5_5.45.bbappend
-
-
-# TODO: check why this is needed
-cp meta-rdk-sagemcom/meta-sagemcom-top-m393-72180ZB-stb/conf/machine/m393-72180ZB-stb.conf meta-rdk-sagemcom/meta-sagemcom-top-m393-72180ZB-stb/conf/machine/m393-72180zb-stb.conf
-sed -i 's/m393-72180ZB-stb/m393-72180zb-stb/' meta-rdk-sagemcom/meta-sagemcom-top-m393-72180ZB-stb/recipes-kernel/sc-devicetree/sc-devicetree.bbappend
-
-# add some extra space
-echo 'IMAGE_ROOTFS_EXTRA_SPACE = "500000"' >> meta-cmf-video-reference/recipes-core/images/rdk-generic-reference-image-brcm.inc
-
 # fix image name in script
 sed -i 's/rdk-generic-mediaclient-image/rdk-generic-reference-image/' meta-rdk-sagemcom/scripts/gen-upgrade-rdk.sh  
 
 cat <<EOF >> _build.sh
 [ -f /opt/rh/devtoolset-7/enable ] && source /opt/rh/devtoolset-7/enable
 
-./meta-rdk-sagemcom/scripts/configure-sagemcom-reference.sh -y dunfell -B 22.0.1 -M m393-72180ZB-stb -D rdkcmf-dev
-
-source ./build-m393-72180ZB-stb-sdk22.0.1-rdkcmf-dev/activate.sh
-
-sed -i 's/INHERIT/#INHERIT/' ../openembedded-core/meta/conf/sanity.conf
-
-sed -i 's/m393-72180ZB-stb/m393-72180zb-stb/' conf/local.conf
-rm conf/auto.conf
-touch conf/auto.conf
-echo "require conf/distro/include/reference.inc" >> conf/local.conf
+declare -x MACHINE="m393-72180ZB-stb"
+. ./meta-cmf-video-reference-next/setup-environment
 
 echo "bitbake -k rdk-generic-reference-image"
 EOF
