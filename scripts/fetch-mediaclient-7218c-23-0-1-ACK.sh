3d212a2086f847a4b4002b2441e041ec9d680be3
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2023-09-05 08:26:37 +0200

    Add patches for DAC-sec

--- scripts/fetch-mediaclient-7218c-23-0-1-ACK.sh
+++ scripts/fetch-mediaclient-7218c-23-0-1-ACK.sh
@@ -70,22 +70,25 @@ function apply_patches() {
     # Patches for DAC-sec functionality
 
     # RDKDEV-774 Enable DAC-sec distro feature in reference images
-    (cd meta-cmf-video && git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video refs/changes/32/90332/2 && git cherry-pick FETCH_HEAD)
+    (cd meta-cmf-video && git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video refs/changes/32/90332/7 && git cherry-pick FETCH_HEAD)
 
     # RDKDEV-774 Add support for DAC-sec distro feature
-    (cd meta-rdk-video && git fetch https://code.rdkcentral.com/r/rdk/components/generic/rdk-oe/meta-rdk-video refs/changes/75/86775/12 && git cherry-pick FETCH_HEAD)
+    (cd meta-rdk-video && git fetch https://code.rdkcentral.com/r/rdk/components/generic/rdk-oe/meta-rdk-video refs/changes/75/86775/14 && git cherry-pick FETCH_HEAD)
 
     # RDKDEV-774 DAC-sec libkwk-rdk-data
-    (cd meta-cmf-restricted && git fetch https://code.rdkcentral.com/r/components/restricted/rdk-oe/meta-cmf-restricted refs/changes/66/90266/3 && git cherry-pick FETCH_HEAD)
+    (cd meta-cmf-restricted && git fetch https://code.rdkcentral.com/r/components/restricted/rdk-oe/meta-cmf-restricted refs/changes/66/90266/4 && git cherry-pick FETCH_HEAD)
 
     # RDKDEV-774 Add runtime dependency for libkwk-data
-    (cd meta-cmf-restricted && git fetch https://code.rdkcentral.com/r/components/restricted/rdk-oe/meta-cmf-restricted refs/changes/30/90330/1 && git cherry-pick FETCH_HEAD)
+    (cd meta-cmf-restricted && git fetch https://code.rdkcentral.com/r/components/restricted/rdk-oe/meta-cmf-restricted refs/changes/30/90330/2 && git cherry-pick FETCH_HEAD)
 
     # RDKDEV-774 Fix calling mContainerStoppedCb()
     (cd meta-cmf-video-reference && git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video-reference refs/changes/65/90265/1 && git cherry-pick FETCH_HEAD)
 
     # RDKCMF-8908 Fix lib32-lvm2 packaging error
-    (cd meta-rdk-ext && git fetch https://code.rdkcentral.com/r/rdk/components/generic/rdk-oe/meta-rdk-ext refs/changes/10/85410/1 && git cherry-pick FETCH_HEAD)
+    #(cd meta-rdk-ext && git fetch https://code.rdkcentral.com/r/rdk/components/generic/rdk-oe/meta-rdk-ext refs/changes/10/85410/2 && git cherry-pick FETCH_HEAD)
+
+    # RDKCMF-8908 Fix lib32-lvm2 packaging error (stopgap)
+    (cd meta-cmf-video && git fetch https://code.rdkcentral.com/r/components/generic/rdk-oe/meta-cmf-video refs/changes/07/90907/1 && git cherry-pick FETCH_HEAD)
 }
 
 # Uncomment to enable patches for DAC-sec
